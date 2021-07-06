c515013757ced0579a1fae7942d1ceaddf4ca50d
vcaselli <vcasellidev@gmail.com>
2021-07-05 21:52:17 -0300

    voltando...

--- hr-api-gateway-zuul/src/main/java/br/com/casellimscourse/apigateway/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/br/com/casellimscourse/apigateway/HrApiGatewayZuulApplication.java
@@ -1,15 +1,19 @@
-package br.com.ddtechrj.hrworker;
+package br.com.casellimscourse.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import jdk.jfr.Enabled;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
-public class HrWorkerApplication {
+public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrWorkerApplication.class, args);
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
 
 }
---
+++ hr-eureka-server/src/test/java/br/com/casellimscourse/eurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casellimscourse.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
